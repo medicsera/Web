1250ebffc517e13b4b2648dbb78627ba11a3da87
medicsera <anton.mirko.0550@gmail.com>
2025-10-09 00:25:31 +0700

    added func for adding new tasks

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    let taskId = 1;
+
+    $('.add-task-form').on('submit', function(e){
+        e.preventDefault();
+        const taskText = $('.add-task-input').val().trim();
+        if (!taskText) return;
+        
+        const newTask = `
+             <div class="task-item" data-id=${taskId}>
+                <div class="task-item-left">
+                    <span class="task-text" contenteditable="false">${$('<div>').text(taskText).html()}</span>
+                </div>
+                <div class="task-item-right">
+                    <input class="task-checkbox" type="checkbox" />
+                    <button class="task-btn" title="Delete">&#128465;</button>
+                </div>
+            </div>
+        `
+        $('.task').append(newTask);
+        $('.add-task-input').val('');
+        taskId++
+    })
+})
